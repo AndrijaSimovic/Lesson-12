ecd00b2b66bf7af4e45ce047cedbc3024a62007a
AndrijaSimovic <andrijasimovic44@gmail.com>
2023-11-23 23:52:25 +0100

    doAppendChild - done

--- js/functions.js
+++ js/functions.js
@@ -3,10 +3,16 @@ function elementGenerator(elementName) {
     return newElement;
 }
 
+// THIS IS HOW I TRIED TO DO IT BEFORE (I left this comment here so we can maybe talk about it later)
+// function doAppendChild(parentElement, childElement) {
+//     const parent = document.getElementById(parentElement);
+//     const child = document.getElementById(childElement);
+//     parent.appendChild(child);
+// }
+
 function doAppendChild(parentElement, childElement) {
-    const parent = document.getElementById(parentElement);
-    const child = document.getElementById(childElement);
-    parent.appendChild(child);
+    parentElement.appendChild(childElement);
 }
+
 
 export { elementGenerator, doAppendChild };
--- js/main.js
+++ js/main.js
@@ -5,52 +5,54 @@ function main() {
     // welcomeText
     const welcomeTextElement = elementGenerator('h1');
     welcomeTextElement.innerText = welcomeText;
+    welcomeTextElement.classList.add('marginLeft15');
     doAppendChild(root, welcomeTextElement);
 
     // workingTime
     const workingTimeElement = elementGenerator('h3');
     workingTimeElement.innerText = workingTime;
+    workingTimeElement.classList.add('marginLeft15');
+    doAppendChild(root, workingTimeElement);
 
     // #region coffeeList
     coffee.forEach((coffeeItem) => {
 
         const coffeeList = document.getElementById('list');
-        coffeeList.classList.add('coffeeList');
+        coffeeList.classList.add('coffeeList', 'marginLeft5');
 
         const coffeeItemDiv = elementGenerator('li');
         coffeeItemDiv.classList.add('row');
-        coffeeList.appendChild(coffeeItemDiv);
+        doAppendChild(coffeeList, coffeeItemDiv);
 
         const coffeeImage = elementGenerator('img');
         coffeeImage.src = coffeeItem.image;
-        coffeeItemDiv.appendChild(coffeeImage);
         coffeeImage.classList.add('coffeeImage');
+        doAppendChild(coffeeItemDiv, coffeeImage);
 
         const coffeeInfoDiv = elementGenerator('div');
-        coffeeInfoDiv.classList.add('column');
-        coffeeInfoDiv.classList.add('coffeeInfoDiv')
-        coffeeItemDiv.appendChild(coffeeInfoDiv);
+        coffeeInfoDiv.classList.add('column', 'marginLeft15');
+        doAppendChild(coffeeItemDiv, coffeeInfoDiv);
 
         const coffeeInfoDivTitle = elementGenerator('h3');
         coffeeInfoDivTitle.innerText = coffeeItem.title;
-        coffeeInfoDiv.appendChild(coffeeInfoDivTitle);
+        doAppendChild(coffeeInfoDiv, coffeeInfoDivTitle)
 
         const coffeeInfoDivDescription = elementGenerator('p');
         coffeeInfoDivDescription.innerText = coffeeItem.description;
-        coffeeInfoDiv.appendChild(coffeeInfoDivDescription);
+        doAppendChild(coffeeInfoDiv, coffeeInfoDivDescription);
 
-        // roast
-        const index = coffeeItem.minRoastTypeValue;
-        while (index <= coffeeItem.maxRoastTypeValue) {
-            const firstDiv = elementGenerator('div');
-            coffeeInfoDiv.appendChild(firstDiv);
-            firstDiv.classList.add('firstDiv');
-            index ++;
-            if (index <= coffeeItem.roastTypeValue) {
-                firstDiv.class.add('');
-            }
-            index ++;
-        }
+        // ROAST - thing to do
+        // const index = coffeeItem.minRoastTypeValue;
+        // while (index <= coffeeItem.maxRoastTypeValue) {
+        //     const firstDiv = elementGenerator('div');
+        //     coffeeInfoDiv.appendChild(firstDiv);
+        //     firstDiv.classList.add('firstDiv');
+        //     index ++;
+        //     if (index <= coffeeItem.roastTypeValue) {
+        //         firstDiv.class.add('');
+        //     }
+        //     index ++;
+        // }
     })
     // #endregion
 }
